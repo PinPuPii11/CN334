c2faf53619fcd32ce1d660e07796cb56d81e741a
PinPuPii11 <pin6229@gmail.com>
2024-01-30 14:26:00 +0700

    Add 5 new page

--- ecommerce/views.py
+++ ecommerce/views.py
@@ -14,3 +14,18 @@ def item_view(request, item_id):
     }
 
     return render(request, 'index.html',context = context_data)
+
+def home_view(req):
+    return render(req, 'homepage.html')
+
+def category_view(req):
+    return render(req, 'category.html')
+
+def product_view(req):
+    return render(req, 'product.html')
+
+def checkout_view(req):
+    return render(req, 'checkout.html')
+
+def contact_view(req):
+    return render(req, 'contact.html')
